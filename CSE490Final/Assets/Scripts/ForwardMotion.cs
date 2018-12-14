c0b74b3570b1a030fc13cb9a4ed462203c2a7cb9
Jose Pacio <pacioj@attu.cs.washington.edu>
2018-12-13 16:57:06 -0800

    forardMotion

---
+++ CSE490Final/Assets/Scripts/ForwardMotion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardMotion : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Vector3 origin;
+    public float distanceReset;
+    public Vector3 direction;
+
+    private void Start()
+    {
+        origin = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(direction * speed * 0.01f);
+        if (Vector3.Distance(origin, transform.position) > distanceReset) {
+           transform.position = origin;
+        }
+    }
+}
